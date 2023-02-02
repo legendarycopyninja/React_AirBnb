c5fc58bd97bae5995bebed0328fb180fc5cd26a7
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2023-02-02 15:20:30 +0530

    commit

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
 import Navbar from './Navbar'
 import Hero from './Hero'
+import Card from './Card'
+import { data } from './data'
+
 
 function App() {
+
+  const dataElements = data.map(function(item) {
+    return <Card 
+    key={item.id}
+    img={item.coverImg}
+    rating ={item.stats.rating}
+    reviewCount={item.stats.reviewCount}
+    location={item.location}
+    title ={item.title}
+    price={item.price}
+    openSpots={item.openSpots} />
+  })
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <section className="cards-list">
+        {dataElements}
+      </section>
     </div>
   )
 }
